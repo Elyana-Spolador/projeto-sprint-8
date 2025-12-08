2fde944e6e2879b21f1ef9ed05bbfa1294602db5
Elyana Spolador <elyana.spolador@gmail.com>
2025-12-08 11:08:05 +0000

    atualizacao projeto

--- main.py
+++ main.py
@@ -14,43 +14,68 @@ class TestUrbanRoutes:
 
 
     def test_set_route(self):
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        address_from = data.ADDRESS_FROM
+        address_to = data.ADDRESS_TO
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
 
 
     def test_select_plan(self):
-        # Adicionar em S8
-        pass
+        def test_select_plan(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        routes_page.select_supportive_plan()
+        assert routes_page.get_current_selected_plan() == 'Comfort'
 
 
     def test_fill_phone_number(self):
-        # Adicionar em S8
-        pass
+        def test_fill_phone_number(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        phone_number = data.PHONE_NUMBER
+        routes_page.set_phone(phone_number)
+        assert routes_page.get_phone() == phone_number
 
 
     def test_fill_card(self):
-        # Adicionar em S8
-        pass
+        def test_fill_card(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        routes_page.set_card(data.CARD_NUMBER, data.CARD_CODE)
+        assert routes_page.get_current_payment_method() == 'Cartão'
 
 
     def test_comment_for_driver(self):
-        # Adicionar em S8
-        pass
+        def test_comment_for_driver(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        message = data.MESSAGE_FOR_DRIVER
+        routes_page.set_message_for_driver(message)
+        assert routes_page.get_message_for_driver() == message
 
 
     def test_order_blanket_and_handkerchiefs(self):
-        # Adicionar em S8
-        pass
+        def test_order_blanket_and_handkerchiefs(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        routes_page.click_blanket_and_handkerchiefs_option()
+        assert routes_page.get_blanket_and_handkerchiefs_option_checked()
 
 
     def test_order_2_ice_creams(self):
-        # Adcionar em S8
-        print("Função para adicionar dois sorvetes")
-        for i in range(2):
-            # Adicionar em S8
-            pass
+        def test_order_2_ice_creams(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        routes_page.add_ice_cream(2)
+        assert routes_page.get_amount_of_ice_cream() == 2
 
 
     def test_car_search_model_appears(self):
-        # Adicionar em S8
-        pass
+        def test_order_2_ice_creams(self):
+            routes_page = UrbanRoutesPage(self.driver)
+
+        routes_page.add_ice_cream(2)
+        assert routes_page.get_amount_of_ice_cream() == 2
